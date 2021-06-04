f4735e9a088fb32438293c08d201b55d76915a85
iffatainiyyah <iffatainiyyah@gmail.com>
2021-06-04 20:37:24 +0800

    fix error for list item task binding

--- app/src/main/java/com/unhas/ac/id/roomdb/crud/mytask/adapter/TaskAdapter.kt
+++ app/src/main/java/com/unhas/ac/id/roomdb/crud/mytask/adapter/TaskAdapter.kt
@@ -4,8 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.unhas.ac.id.roomdb.crud.mytask.databinding.ListItemBinding
-import com.unhas.ac.id.roomdb.crud.mytask.R 
+import com.unhas.ac.id.roomdb.crud.mytask.R
+import com.unhas.ac.id.roomdb.crud.mytask.databinding.ListitemTaskBinding
 import com.unhas.ac.id.roomdb.crud.mytask.db.Task
 
 
@@ -13,7 +13,7 @@ class TaskAdapter(private val taskList: List<Task>, private val clickListener: (
     : RecyclerView.Adapter<TaskViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding: ListItemBinding = DataBindingUtil.inflate(layoutInflater, R.layout.listitem_task, parent, false)
+        val binding: ListitemTaskBinding = DataBindingUtil.inflate(layoutInflater, R.layout.listitem_task, parent, false)
         return TaskViewHolder(binding)
     }
 
@@ -26,12 +26,12 @@ class TaskAdapter(private val taskList: List<Task>, private val clickListener: (
     }
 
 }
-class TaskViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root) {
+class TaskViewHolder(val binding: ListitemTaskBinding): RecyclerView.ViewHolder(binding.root) {
     fun bind(task: Task, clickListener: (Task)->Unit) {
-        binding.nameTextView.text = task.name
-        binding.dateTextView.text = task.date
+        binding.nameTextview.text = task.name
+        binding.dateTextview.text = task.date
 
-        binding.listItemLayout.setOnClickListener{
+        binding.listitemLayout.setOnClickListener{
             clickListener(task)
         }
     }
